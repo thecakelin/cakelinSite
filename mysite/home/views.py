8c530b092281bd6b129540b5b22f34e08c8bfd8c
kaitlinmarquardt <kaitlinmarquardt@gmail.com>
2017-08-15 00:41:53 -0500

    Updated database and homepage routing

---
+++ mysite/home/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+# def index(request):
+#     return render_to_response('home/index.html')
+
+# def index(request):
+#      return HttpResponse("Coming Soon!")
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
