ca3b847691c51ce03a457ef590b18a7579c4de80
Kaitlin Marquardt <kaitlinmarquardt@gmail.com>
2017-08-19 22:48:49 +0000

    Static file changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', include('home.urls'))
+    url(r'^', include('home.urls'))
     # url(r'^$', 'home.views.index'),
 ]
